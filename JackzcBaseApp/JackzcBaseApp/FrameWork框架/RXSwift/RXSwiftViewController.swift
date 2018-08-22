007db8e34c1a28b49a9150db33181325c4cad6a6
jackzc <920271619@qq.com>
2018-08-22 21:36:40 +0800

    添加RxSwift

--- JackzcBaseApp/JackzcBaseApp/FrameWork框架/RXSwift/RXSwiftViewController.swift
+++ JackzcBaseApp/JackzcBaseApp/FrameWork框架/RXSwift/RXSwiftViewController.swift
@@ -8,29 +8,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RXSwiftViewController: AppBaseViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+struct StudentModel {
+    var name:String
+    var age:Int
+    init(name:String, age:Int) {
+        self.name = name
+        self.age = age
     }
-    */
+    mutating func showName(name:String){
+        self.name = name
+    }
+}
 
+extension StudentModel:CustomStringConvertible{
+    var description: String {
+        return "name == \(self.name) age == \(self.age)"
+    }
 }
